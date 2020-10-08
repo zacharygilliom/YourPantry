365105b4c6718198f63a7136785d7fb49d72e21d
Zachary Gilliom <zacharygilliom@gmail.com>
2020-10-07 22:44:04 -0400

    Added CLI functionality to main.go, added functions to database.go to view data and add data and remove data.

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/zacharygilliom/internal/database"
-	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func main() {
+	fmt.Println("Connection Started...")
 	client, ctx, err := database.CreateConnection()
 	err = client.Connect(ctx)
 	if err != nil {
@@ -17,32 +23,65 @@ func main() {
 
 	//pantryDatabase := client.Database("pantry")
 	databaseName := "pantry"
-	pantryDatabase := database.CreateDatabase(databaseName, client)
+	pantryDatabase := database.NewDatabase(databaseName, client)
 
 	//pantryIngredient := pantryDatabase.Collection("ingredient")
-	//ingredientCollection := "ingredient"
-	//pantryIngredient := database.CreateCollection(ingredientCollection, pantryDatabase)
+	ingredientCollection := "ingredient"
+	pantryIngredient := database.NewCollection(ingredientCollection, pantryDatabase)
 
-	userCollection := "user"
-	pantryUser := database.CreateCollection(userCollection, pantryDatabase)
+	//userCollection := "user"
+	//pantryUser := database.CreateCollection(userCollection, pantryDatabase)
 
-	data := bson.D{
-		{"firstname", "Zach"},
-		{"lastname", "Gilliom"},
-		{"email", "zacharygilliom@gmail.com"},
-	}
-	database.InsertDataToCollection(pantryUser, ctx, data)
 	//pantryResult, err := pantryIngredient.InsertOne(ctx, bson.D{
-	//{"name", "flour"},
+	//	{"name", "flour"},
 	//})
 	//if err != nil {
 	//	log.Fatal(err)
 	//}
-	//fmt.Println(pantryResult)
+	//fmt.Println(pantryResult.InsertedID)
+	userChoice := AppMenu()
+	AppSelection(userChoice, pantryIngredient, ctx)
+	ing := "Jam"
 
+	database.InsertDataToCollection(pantryIngredient, ctx, ing)
 }
 
-func getUserInput() string {
-	var answer string
-	return answer
+func AppMenu() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Your Pantry Application")
+	fmt.Println("-----------------------")
+	fmt.Println("Please select an action")
+	fmt.Println("1. Add Ingredient")
+	fmt.Println("2. Remove Ingredient")
+	fmt.Println("3. View Ingredients")
+	fmt.Println("4. Search Recipes")
+	text, _ := reader.ReadString('\n')
+	text = strings.Replace(text, "\n", "", -1)
+	return text
+}
+
+func AppSelection(choice string, collection *mongo.Collection, ctx context.Context) {
+	switch choice {
+	case "1":
+		fmt.Println("Please type the ingredient to add...")
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		text = strings.Replace(text, "\n", "", -1)
+		database.InsertDataToCollection(collection, ctx, text)
+		userChoice := AppMenu()
+		AppSelection(userChoice, collection, ctx)
+	case "2":
+		fmt.Println("Please type the ingredient to remove...")
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		text = strings.Replace(text, "\n", "", -1)
+		database.RemoveManyFromCollection(collection, ctx, text)
+		userChoice := AppMenu()
+		AppSelection(userChoice, collection, ctx)
+	case "3":
+		database.ListDocuments(collection, ctx)
+		userChoice := AppMenu()
+		AppSelection(userChoice, collection, ctx)
+
+	}
 }
--- internal/database/database.go
+++ internal/database/database.go
@@ -2,30 +2,32 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type User struct {
-	ID        primitive.ObjectID `bson:"_id, omitempty"`
-	Firstname string             `bson:"firstname, omitempty"`
-	Lastname  string             `bson:"lastname, omitempty"`
-	Email     string             `bson:"email, omitempty"`
-}
-
-type IngredientList struct {
-	ID         primitive.ObjectID `bson:"_id, omitempty"`
-	User       primitive.ObjectID `bson:"user, omitempty"`
-	Ingredient []Ingredient       `bson:"ingredient"`
-}
-
+//type User struct {
+//	ID        primitive.ObjectID `bson:"_id, omitempty"`
+//	Firstname string             `bson:"firstname, omitempty"`
+//	Lastname  string             `bson:"lastname, omitempty"`
+//	Email     string             `bson:"email, omitempty"`
+//}
+//
+//type IngredientList struct {
+//	ID         primitive.ObjectID `bson:"_id, omitempty"`
+//	User       primitive.ObjectID `bson:"user, omitempty"`
+//	Ingredient []Ingredient       `bson:"ingredient"`
+//}
+//
 type Ingredient struct {
-	//ID   primitive.ObjectID `bson:"_id"`
-	Name string `bson:"name, omitempty"`
+	ID   primitive.ObjectID `bson:"_id, omitempty"`
+	Name string             `bson:"name, omitempty"`
 }
 
 func CreateConnection() (*mongo.Client, context.Context, error) {
@@ -33,23 +35,61 @@ func CreateConnection() (*mongo.Client, context.Context, error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
 	return client, ctx, err
 }
 
-func CreateDatabase(databaseName string, client *mongo.Client) *mongo.Database {
+func NewDatabase(databaseName string, client *mongo.Client) *mongo.Database {
 	database := client.Database(databaseName)
 	return database
 }
 
-func CreateCollection(collectionName string, database *mongo.Database) *mongo.Collection {
+func NewCollection(collectionName string, database *mongo.Database) *mongo.Collection {
 	collection := database.Collection(collectionName)
 	return collection
 }
 
-func InsertDataToCollection(collection *mongo.Collection, ctx context.Context, data primitive.D) {
-	_, err := collection.InsertOne(ctx, data)
+func InsertDataToCollection(collection *mongo.Collection, ctx context.Context, data string) {
+	ingredient := bson.D{
+		{"name", data},
+	}
+	result, err := collection.InsertOne(ctx, ingredient)
 	if err != nil {
 		log.Fatal(err)
+	}
+	fmt.Println(result.InsertedID)
+}
+
+func RemoveManyFromCollection(collection *mongo.Collection, ctx context.Context, data string) {
+	filter := bson.D{
+		{"name", data},
+	}
+	result, err := collection.DeleteMany(ctx, filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if result.DeletedCount > 1 {
+		fmt.Printf("%v instances of %v deleted", result.DeletedCount, data)
+	} else {
+		fmt.Printf("%v instance of %v deleted", result.DeletedCount, data)
+	}
+	fmt.Println("")
+	fmt.Println("")
+}
+
+func ListDocuments(collection *mongo.Collection, ctx context.Context) {
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		for cursor.Next(ctx) {
+			var result Ingredient
+			err := cursor.Decode(&result)
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				fmt.Println(result.Name)
+			}
+		}
 	}
 }
