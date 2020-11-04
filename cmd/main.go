a9af5e7b49409b14b26149d90dd4bdc21dd70ab2
Zachary Gilliom <zacharygilliom@gmail.com>
2020-11-03 20:33:27 -0500

    Added cli internal file to handle all the cli functions, and moved the cli function from main.go into cli.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,13 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"time"
 
+	"github.com/zacharygilliom/internal/cli"
 	"github.com/zacharygilliom/internal/database"
-	"go.mongodb.org/mongo-driver/mongo"
 )
-
-type RecipeList struct {
-	List []Recipe `json:"results"`
-}
-
-type Recipe struct {
-	ID                  int    `json:"id"`
-	UsedIngredientCount int    `json:"usedIngredientCount"`
-	Title               string `json:"title"`
-}
 
 func main() {
 	fmt.Println("Connection Started...")
@@ -43,75 +31,6 @@ func main() {
 	userData := database.GetUserInfo()
 	userID := database.InsertDataToUsers(pantryUser, userData)
 
-	userChoice := appMenu()
-	appSelection(userChoice, pantryIngredient, userID)
-}
-
-func appMenu() string {
-	fmt.Println("Your Pantry Application")
-	fmt.Println("-----------------------")
-	fmt.Println("Please select an action")
-	fmt.Println("1. Add Ingredient")
-	fmt.Println("2. Remove Ingredient")
-	fmt.Println("3. View Ingredients")
-	fmt.Println("4. Search Recipes")
-	fmt.Println("5. Close application")
-	fmt.Println("-----------------------")
-	text := getUserInput()
-	return text
-}
-
-func appSelection(choice string, collection *mongo.Collection, userID interface{}) {
-	switch choice {
-	case "1":
-		fmt.Println("Please type the ingredient to add...")
-		text := getUserInput()
-		database.InsertDataToIngredients(collection, userID, text)
-		userChoice := appMenu()
-		appSelection(userChoice, collection, userID)
-	case "2":
-		fmt.Println("Please type the ingredient to remove...")
-		text := getUserInput()
-		database.RemoveManyFromIngredients(collection, userID, text)
-		userChoice := appMenu()
-		appSelection(userChoice, collection, userID)
-	case "3":
-		database.ListDocuments(collection, userID)
-		userChoice := appMenu()
-		appSelection(userChoice, collection, userID)
-	case "4":
-		SearchIngredients(collection, userID)
-		userChoice := appMenu()
-		appSelection(userChoice, collection, userID)
-	case "5":
-		fmt.Println("Application Closed")
-	}
-}
-
-func getUserInput() string {
-	var text string
-	fmt.Scanf("%s", &text)
-	return text
-}
-
-func SearchIngredients(collection *mongo.Collection, userID interface{}) {
-	ingred := database.BuildIngredientString(collection, userID)
-	fmt.Println(ingred)
-	resp, err := http.Get("https://api.spoonacular.com/recipes/complexSearch?apiKey=58bbec758ee847f7b331410b02c7252d&findByIngredients=" + ingred + "&number=10")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	var r RecipeList
-
-	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(r)
-
-	for _, rec := range r.List {
-		fmt.Println(rec.Title)
-	}
+	userChoice := cli.Menu()
+	cli.Selection(userChoice, pantryIngredient, userID)
 }
---
+++ internal/cli/cli.go
@@ -0,0 +1,89 @@
+package cli
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/zacharygilliom/internal/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type RecipeList struct {
+	List []Recipe `json:"results"`
+}
+
+type Recipe struct {
+	ID                  int    `json:"id"`
+	UsedIngredientCount int    `json:"usedIngredientCount"`
+	Title               string `json:"title"`
+}
+
+func Menu() string {
+	fmt.Println("Your Pantry Application")
+	fmt.Println("-----------------------")
+	fmt.Println("Please select an action")
+	fmt.Println("1. Add Ingredient")
+	fmt.Println("2. Remove Ingredient")
+	fmt.Println("3. View Ingredients")
+	fmt.Println("4. Search Recipes")
+	fmt.Println("5. Close application")
+	fmt.Println("-----------------------")
+	text := getUserInput()
+	return text
+}
+
+func getUserInput() string {
+	var text string
+	fmt.Scanf("%s", &text)
+	return text
+}
+
+func Selection(choice string, collection *mongo.Collection, userID interface{}) {
+	switch choice {
+	case "1":
+		fmt.Println("Please type the ingredient to add...")
+		text := getUserInput()
+		database.InsertDataToIngredients(collection, userID, text)
+		userChoice := Menu()
+		Selection(userChoice, collection, userID)
+	case "2":
+		fmt.Println("Please type the ingredient to remove...")
+		text := getUserInput()
+		database.RemoveManyFromIngredients(collection, userID, text)
+		userChoice := Menu()
+		Selection(userChoice, collection, userID)
+	case "3":
+		database.ListDocuments(collection, userID)
+		userChoice := Menu()
+		Selection(userChoice, collection, userID)
+	case "4":
+		SearchIngredients(collection, userID)
+		userChoice := Menu()
+		Selection(userChoice, collection, userID)
+	case "5":
+		fmt.Println("Application Closed")
+	}
+}
+
+func SearchIngredients(collection *mongo.Collection, userID interface{}) {
+	ingred := database.BuildIngredientString(collection, userID)
+	resp, err := http.Get("https://api.spoonacular.com/recipes/complexSearch?apiKey=58bbec758ee847f7b331410b02c7252d&findByIngredients=" + ingred + "&number=10")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	var r RecipeList
+
+	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(r)
+
+	for _, rec := range r.List {
+		fmt.Println(rec.Title)
+	}
+}
