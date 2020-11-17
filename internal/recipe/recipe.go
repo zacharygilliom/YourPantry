2b6e5507fa317a097a35618727d3292505800104
Zachary Gilliom <zacharygilliom@gmail.com>
2020-11-16 20:51:33 -0500

    Added struct in recipe.go

--- internal/recipe/recipe.go
+++ internal/recipe/recipe.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+type Recipe struct {
+	Name string `json:"name"`
+}
+
 func getRecipes(ingredients string) {
 	resp, err := http.Get("https://api.edamam.com/search/?q=" + ingredients + "app_id=d18be80e&app_key=3d1fe9b7d97a890ac8e4d47c3d54fa88")
 	if err != nil {
