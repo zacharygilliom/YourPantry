6ceb1e45e2a352c7b6577df2c145143ec0762a41
zacharygilliom <zacharygilliom@gmail.com>
2021-11-28 23:58:30 -0500

    added route to add ingredient

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,48 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zacharygilliom/internal/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 func main() {
+	fmt.Println("Connection Started...")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	client, err := database.CreateConnection(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	fmt.Println("Connection Established")
+
+	databaseName := "pantry"
+	pantryDatabase := database.NewDatabase(databaseName, client)
+
+	userCollection := "user"
+	pantryUser := database.NewCollection(userCollection, pantryDatabase)
+
+	ingredientCollection := "ingredient"
+	pantryIngredient := database.NewCollection(ingredientCollection, pantryDatabase)
+
+	userData := database.GetUserInfo()
+	userID := database.InsertDataToUsers(pantryUser, userData)
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/:ingredient", addIngredient(c*gin.Context, pantryIngredient))
+
 	r.Run()
+}
+
+func addIngredients(c *gin.Context, collection *mongo.Collection) {
+	database.InsertDataToIngredients(collection, userID, "chicken")
 }
 
 /*
