97b6651788aab86fdc7f4b536c29ebad295b9f89
zacharygilliom <zacharygilliom@gmail.com>
2022-02-08 00:59:13 -0500

    updated js file to add list to index page.

--- frontend/js/src/main.js
+++ frontend/js/src/main.js
@@ -2,13 +2,23 @@ async function fetchIngredList() {
 	try {
 		let response =  await fetch('http://localhost:8080/61ece6d2e84c62bdcdbcc42d/ingredients/list');
 		let data = await response.json();
-		console.log(data["ingredients"])
-		for (let i = 0; i < data['ingredients'].length; i++) {
-			console.log(data['ingredients'][i])
+		/*let data = await {'ingredients': [],
 		}
+		*/
+		var str = '<ol>'
+		data['ingredients'].forEach(function(ingredient) {
+			str += '<li>' + ingredient + '</li>'
+		});
+		str += '</ol>';
+		document.getElementById('ingredient-list').innerHTML = str;
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-fetchIngredList()
+
+function main() {
+	fetchIngredList()
+}
+
+main()
