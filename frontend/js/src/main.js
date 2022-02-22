f76f343a3e4e0e9595cf2304a92eb0bec60bd308
zacharygilliom <zacharygilliom@gmail.com>
2022-02-21 23:32:31 -0500

    Added login form in html, and added login user function in js.

--- frontend/js/src/main.js
+++ frontend/js/src/main.js
@@ -16,9 +16,34 @@ async function fetchIngredList() {
 	}
 }
 
+async function signUpUser() {
+	try {
+		username = document.getElementById('sign-up-email').value;	
+		password = document.getElementById('sign-up-password').value;
+		let response = await fetch('http://localhost:8080/username/password');
+		let data = await response.json();
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function loginUser() {
+	try {
+		username = document.getElementById('login-email').value;
+		password = document.getElementById('login-password').value;
+		var apiUrl = "http://localhost:8080/login/" + username + "/" + password
+		console.log(apiUrl);
+		let response = await fetch(apiUrl);
+		console.log(response);
+		let data = await response.json();
+		console.log(data)
+	} catch (error) {
+		console.log(error);
+	}
+}
 
 function main() {
-	fetchIngredList()
+	//fetchIngredList()
 }
 
 main()
